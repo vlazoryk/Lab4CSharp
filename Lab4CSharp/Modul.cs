c70b5c716f001f8b8824de6fc8dfd06f3ccd2827
vlazoryk <v.lazoryk@chnu.edu.ua>
2022-09-29 20:56:30 +0300

    Добавьте файлы проекта.

---
+++ Lab4CSharp/Modul.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4CSharp
+{
+    internal class UserClass
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Lab4CSharp/Program.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+/// <summary>
+///  Top-level statements 
+///  Код програми (оператори)  вищого рівня
+/// </summary>
+///
+Console.WriteLine("Lab4 C# ");
+AnyFunc();
+
+/// <summary>
+/// 
+///  Top-level statements must precede namespace and type declarations.
+/// At the top-level methods/functions can be defined and used
+/// На верхньому рівні можна визначати та використовувати методи/функції
+/// </summary>
+void AnyFunc()
+{
+    Console.WriteLine(" Some function in top-level");
+}
+Console.WriteLine("Problems 1 ");
+AnyFunc();
+//  приклад класів
+UserClass cl = new UserClass();
+cl.Name = " UserClass top-level ";
+User.UserClass cl2 = new User.UserClass();
+cl2.Name = " UserClass namespace User ";
+Lab4CSharp.UserClass cl3 = new Lab4CSharp.UserClass();
+cl3.Name = " UserClass namespace Lab4CSharp ";
+Console.WriteLine(cl + "   " + cl2 + "  " +cl3);
+
+
+
+/// <summary>
+/// 
+/// Top-level statements must precede namespace and type declarations.
+/// Оператори верхнього рівня мають передувати оголошенням простору імен і типу.
+/// Створення класу(ів) або оголошенням простору імен є закіченням  іструкцій верхнього рівня
+/// 
+/// </summary>
+
+namespace User
+{
+    class UserClass
+    {
+        public string Name { get; set; }
+    }
+
+}
+class UserClass
+{
+    public string Name { get; set; }
+}
