2520b6981cf4da0a1e27815c0933288d71c92be7
Vasyl <vlazoryk@chnu.edu.ua>
2024-02-12 08:47:53 +0200

    New reg

--- Lab4CSharp/Program.cs
+++ Lab4CSharp/Program.cs
@@ -22,11 +22,9 @@ AnyFunc();
 //  приклад класів
 UserClass cl = new UserClass();
 cl.Name = " UserClass top-level ";
-User.UserClass cl2 = new User.UserClass();
-cl2.Name = " UserClass namespace User ";
-Lab4CSharp.UserClass cl3 = new Lab4CSharp.UserClass();
-cl3.Name = " UserClass namespace Lab4CSharp ";
-Console.WriteLine(cl + "   " + cl2 + "  " +cl3);
+Lab4CSharp.UserClass cl2 = new Lab4CSharp.UserClass();
+cl2.Name = " UserClass namespace Lab4CSharp ";
+Console.WriteLine(cl + "   " + cl2 + "   ");
 
 
 
@@ -38,23 +36,7 @@ Console.WriteLine(cl + "   " + cl2 + "  " +cl3);
 /// 
 /// </summary>
 
-namespace User
-{
-    class UserClass
-    {
-        public string Name { get; set; }
-        UserClass()
-        {
-            Name = "NoName";
-        }
-        UserClass(string n)
-        {
-            Name = n;
-        }
-    }
-
-}
 class UserClass
 {
     public string Name { get; set; }
-}
+};
